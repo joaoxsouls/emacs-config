843f5dc53905469e95000a506ab436793b619db3
joaoxsouls <joaoxsouls@gmail.com>
2012-07-10 00:57:39 +0100

    gitignore file changes

---
+++ configs/mode-hooks.el
@@ -0,0 +1,30 @@
+
+;javascript mode hooks
+(add-hook 'javascript-mode-hook
+          (lambda ()
+            ;; Scan the file for nested code blocks
+            (imenu-add-menubar-index)
+            ;; Activate the folding mode
+            (hs-minor-mode t)))
+
+;html-mode hooks
+(add-hook 'html-mode-hook
+ (lambda ()
+ (global-set-key (kbd "s-b") 'sgml-skip-tag-backward)
+ (global-set-key (kbd "s-e") 'sgml-skip-tag-forward)
+ (global-set-key (kbd "s-d") 'sgml-delete-tag)
+ ))
+
+;iswitch hooks
+(defun iswitchb-local-keys ()
+  (mapc (lambda (K) 
+	(let* ((key (car K)) (fun (cdr K)))
+	  (define-key iswitchb-mode-map (edmacro-parse-keys key) fun)))
+      '(("<right>" . iswitchb-next-match)
+	("<left>"  . iswitchb-prev-match)
+	("<up>"    . ignore             )
+	("<down>"  . ignore             ))))
+(add-hook 'iswitchb-define-mode-map-hook 'iswitchb-local-keys)
+
+
+(provide 'mode-hooks)
--- init.el
+++ init.el
@@ -7,6 +7,7 @@
 ;config files
 (require 'key-bindings)
 (require 'customizations)
+(require 'mode-hooks)
 
 ;copy text from emacs to external app
 (require 'pbcopy)
@@ -60,27 +61,3 @@
 ;mouse support on cli
 (xterm-mouse-mode)
 
-;;JS-MODE configurations
-(add-hook 'javascript-mode-hook
-          (lambda ()
-            ;; Scan the file for nested code blocks
-            (imenu-add-menubar-index)
-            ;; Activate the folding mode
-            (hs-minor-mode t)))
-;;HTML-MODE configurations
-(add-hook 'html-mode-hook
- (lambda ()
- (global-set-key (kbd "s-b") 'sgml-skip-tag-backward)
- (global-set-key (kbd "s-e") 'sgml-skip-tag-forward)
- (global-set-key (kbd "s-d") 'sgml-delete-tag)
- ))
-
-(defun iswitchb-local-keys ()
-  (mapc (lambda (K) 
-	(let* ((key (car K)) (fun (cdr K)))
-	  (define-key iswitchb-mode-map (edmacro-parse-keys key) fun)))
-      '(("<right>" . iswitchb-next-match)
-	("<left>"  . iswitchb-prev-match)
-	("<up>"    . ignore             )
-	("<down>"  . ignore             ))))
-(add-hook 'iswitchb-define-mode-map-hook 'iswitchb-local-keys)
