ef29252b5c472df58c9149fcf18991c00cbe7faf
joaoxsouls <joaoxsouls@gmail.com>
2013-04-05 15:24:22 +0100

    disable sass mode compilation on save; update recentf key bind to use ido-find-file

--- configs/key-bindings.el
+++ configs/key-bindings.el
@@ -1,68 +1,4 @@
-;GLOBAL KEYS
-
-(global-set-key (kbd "M-n") 'backward-paragraph)
-
-(global-set-key (kbd "M-<up>") 'backward-paragraph)
-
-(global-set-key (kbd "M-m") 'forward-paragraph)
-
-(global-set-key (kbd "M-<down>") 'forward-paragraph)
-
-(global-set-key (kbd "M-<right>") 'forward-word)
-
-(global-set-key (kbd "M-<left>") 'backward-word)
-
-(global-set-key (kbd "C-<up>") 'beginning-of-buffer)
-
-(global-set-key (kbd "C-<down>") 'end-of-buffer)
-
-(global-set-key (kbd "C-x x") 'next-multiframe-window)
-
-(global-set-key (kbd "C-x w") 'delete-window)
-
-(global-set-key (kbd "C-z") 'undo-only)
-
-(global-set-key (kbd "C-x C-f") 'lusty-file-explorer)
-
-(global-set-key (kbd "C-u") 'simp-project-find-file)
-
-(global-set-key (kbd "C-c <right>") 'next-multiframe-window)
-
-(global-set-key (kbd "C-c <left>") 'previous-multiframe-window)
-
-(global-set-key (kbd "C-a") 'mark-whole-buffer)
-
-(global-set-key (kbd "C-<right>") 'end-of-visual-line)
-(global-set-key (kbd "C-<left>") 'beginning-of-visual-line)
-
-(global-set-key (kbd "C-x u") 'undo-only)
-
-(global-set-key (kbd "C-x k") 'kill-this-buffer)
-
-(global-set-key (kbd "s-d") 'hs-toggle-hiding)
-
-(global-set-key (kbd "s-r") 'replace-string)
-
-(global-set-key (kbd "s-u") 'lusty-file-explorer)
-
-(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
-
-(global-set-key (kbd "C-n") 'dirtree-show)
-
-(global-set-key (kbd "C-x b")   'lusty-buffer-explorer)
-
-(global-set-key (kbd "C-c s") 'desktop-save)
-
-(global-set-key (kbd "C-c l") 'desktop-change-dir)
-
-(global-set-key (kbd "C-c C-s") 'split-window-horizontally)
-
-(global-set-key (kbd "C-c C-r") 'recentf-open-files)
-
-(global-set-key (kbd "C-M-<down>") 'scroll-up-command)
-
-(global-set-key (kbd "C-M-<up>") 'scroll-down-command)
-
+;KEY-BINDINGS
 
 ;new line after and before the current
 (defun open-line-below ()
@@ -133,3 +69,77 @@
  (local-set-key (kbd "C-d") 'jedi:goto-definition)
  (local-set-key (kbd "C-M-i") 'jedi:complete))
 )
+
+;ido recentfiles
+(defun recentf-ido-find-file ()
+  "Find a recent file using ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+
+(global-set-key (kbd "C-c C-r") 'recentf-ido-find-file)
+
+
+;GLOBAL KEYS
+
+(global-set-key (kbd "M-n") 'backward-paragraph)
+
+(global-set-key (kbd "M-<up>") 'backward-paragraph)
+
+(global-set-key (kbd "M-m") 'forward-paragraph)
+
+(global-set-key (kbd "M-<down>") 'forward-paragraph)
+
+(global-set-key (kbd "M-<right>") 'forward-word)
+
+(global-set-key (kbd "M-<left>") 'backward-word)
+
+(global-set-key (kbd "C-<up>") 'beginning-of-buffer)
+
+(global-set-key (kbd "C-<down>") 'end-of-buffer)
+
+(global-set-key (kbd "C-x x") 'next-multiframe-window)
+
+(global-set-key (kbd "C-x w") 'delete-window)
+
+(global-set-key (kbd "C-z") 'undo-only)
+
+(global-set-key (kbd "C-x C-f") 'lusty-file-explorer)
+
+(global-set-key (kbd "C-u") 'simp-project-find-file)
+
+(global-set-key (kbd "C-c <right>") 'next-multiframe-window)
+
+(global-set-key (kbd "C-c <left>") 'previous-multiframe-window)
+
+(global-set-key (kbd "C-a") 'mark-whole-buffer)
+
+(global-set-key (kbd "C-<right>") 'end-of-visual-line)
+(global-set-key (kbd "C-<left>") 'beginning-of-visual-line)
+
+(global-set-key (kbd "C-x u") 'undo-only)
+
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
+
+(global-set-key (kbd "s-d") 'hs-toggle-hiding)
+
+(global-set-key (kbd "s-r") 'replace-string)
+
+(global-set-key (kbd "s-u") 'lusty-file-explorer)
+
+(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
+
+(global-set-key (kbd "C-n") 'dirtree-show)
+
+(global-set-key (kbd "C-x b")   'lusty-buffer-explorer)
+
+(global-set-key (kbd "C-c s") 'desktop-save)
+
+(global-set-key (kbd "C-c l") 'desktop-change-dir)
+
+(global-set-key (kbd "C-c C-s") 'split-window-horizontally)
+
+(global-set-key (kbd "C-M-<down>") 'scroll-up-command)
+
+(global-set-key (kbd "C-M-<up>") 'scroll-down-command)
--- init.el
+++ init.el
@@ -47,6 +47,7 @@
 ;sass
 (require 'scss-mode)
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(setq scss-compile-at-save nil)
 
 ;yasnippet
 (require 'yasnippet)
