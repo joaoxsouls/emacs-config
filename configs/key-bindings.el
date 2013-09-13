1678f0930f170acdbbdb020a6eb9eb72aa3af375
joaoxsouls <joaoxsouls@gmail.com>
2013-09-13 03:03:28 +0100

    initial commit

---
+++ configs/key-bindings.el
@@ -0,0 +1,226 @@
+;;new line after and before the current
+(defun open-line-below ()
+  (interactive)
+  (save-excursion
+    (end-of-line)
+    (newline)
+    (indent-for-tab-command)))
+
+(defun open-line-above ()
+
+  (interactive)
+  (save-excursion
+    (forward-line -1)
+    (end-of-line)
+    (newline)
+    (indent-for-tab-command)))
+
+(global-set-key (kbd "C-c C-b") 'open-line-below)
+(global-set-key (kbd "C-c C-a") 'open-line-above)
+
+;;rename files
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+
+;;delete files
+(defun delete-current-buffer-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
+
+(defun comment-or-uncomment-region-or-line ()
+  "comment or uncomment a region if selected, otherwise the whole line"
+  (interactive)
+  (save-excursion
+    (if (region-active-p)
+        (comment-or-uncomment-region (region-beginning) (region-end))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position)))))
+(global-set-key (kbd "C-d") 'comment-or-uncomment-region-or-line)
+
+;;copy region or line
+(defun copy-region-or-whole-line (arg)
+  (interactive "p")
+  (save-excursion)
+  (if (region-active-p)
+      (kill-ring-save (region-beginning) (region-end))
+    (kill-ring-save (line-beginning-position) (line-end-position))))
+(global-set-key (kbd "M-w") 'copy-region-or-whole-line)
+
+;;kill region or line
+(defun kill-region-or-line (&optional arg)
+  (interactive "p")
+  (save-excursion
+    (if (region-active-p)
+        (kill-region (region-beginning) (region-end))
+      (kill-region (line-beginning-position) (line-end-position)))))
+(global-set-key (kbd "C-w") 'kill-region-or-line)
+
+(defun delete-region-or-whole-line(&optional arg)
+  (interactive "p")
+  (save-excursion
+    (if (region-active-p)
+        (delete-region (region-beginning) (region-end))
+      (delete-region (line-beginning-position) (line-end-position)))))
+(global-set-key (kbd "C-k") 'delete-region-or-whole-line)
+
+(defun delete-whole-line()
+  (interactive)
+  (delete-region (line-beginning-position) (line-beginning-position 2)))
+(global-set-key (kbd "M-k") 'delete-whole-line)
+
+;;clean-up and ident
+(defun cleanup-buffer ()
+  (interactive)
+  (cleanup-buffer-safe)
+  (indent-region (point-min) (point-max)))
+
+(defun indent-buffer ()
+  "Indent the currently visited buffer."
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun indent-region-or-buffer ()
+  "Indent a region if selected, otherwise the whole buffer."
+  (interactive)
+  (save-excursion
+    (if (region-active-p)
+        (progn
+          (indent-region (region-beginning) (region-end))
+          (message "Indented selected region."))
+      (progn
+        (indent-buffer)
+        (message "Indented buffer.")))))
+
+(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
+
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+
+;;ido recentfiles
+(defun recentf-ido-find-file ()
+  "Find a recent file using ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+
+(global-set-key (kbd "C-c C-r") 'recentf-ido-find-file)
+
+;;python-mode keys
+;;hook functions
+(add-hook 'python-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c C-c") 'comment-or-uncomment-region-or-line)
+            (local-set-key (kbd "C-c C-r") 'recentf-ido-find-file)
+            (local-set-key (kbd "C-c C-p") 'simp-project-find-file)
+            ))
+
+;;html-mode hooks
+(add-hook 'html-mode-hook
+          (lambda ()
+            (local-set-key (kbd "s-b") 'sgml-skip-tag-backward)
+            (local-set-key (kbd "s-e") 'sgml-skip-tag-forward)
+            (local-set-key (kbd "s-d") 'sgml-delete-tag)
+            (local-set-key (kbd "C-c <right>") 'next-multiframe-window)
+            (local-set-key (kbd "C-c <left>") 'previous-multiframe-window)
+            (local-set-key (kbd "C-c C-c") 'comment-or-uncomment-region-or-line)
+            ))
+
+;;GLOBAL KEYS
+
+(global-set-key (kbd "M-n") 'backward-paragraph)
+
+(global-set-key (kbd "M-<up>") 'backward-paragraph)
+
+(global-set-key (kbd "M-m") 'forward-paragraph)
+
+(global-set-key (kbd "M-<down>") 'forward-paragraph)
+
+(global-set-key (kbd "M-<right>") 'forward-word)
+
+(global-set-key (kbd "M-<left>") 'backward-word)
+
+(global-set-key (kbd "C-<up>") 'beginning-of-buffer)
+
+(global-set-key (kbd "C-<down>") 'end-of-buffer)
+
+(global-set-key (kbd "C-x x") 'next-multiframe-window)
+
+(global-set-key (kbd "C-x w") 'delete-window)
+
+(global-set-key (kbd "C-z") 'undo-only)
+
+(global-set-key (kbd "C-x C-f") 'lusty-file-explorer)
+
+(global-set-key (kbd "C-c <right>") 'next-multiframe-window)
+
+(global-set-key (kbd "C-c <left>") 'previous-multiframe-window)
+
+(global-set-key (kbd "C-a") 'mark-whole-buffer)
+
+(global-set-key (kbd "C-<right>") 'end-of-visual-line)
+
+(global-set-key (kbd "C-<left>") 'beginning-of-visual-line)
+
+(global-set-key (kbd "C-x u") 'undo-only)
+
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
+
+(global-set-key (kbd "s-r") 'replace-string)
+
+(global-set-key (kbd "s-u") 'lusty-file-explorer)
+
+(global-set-key (kbd "C-x b") 'lusty-buffer-explorer)
+
+(global-set-key (kbd "C-x C-b") 'lusty-buffer-explorer)
+
+(global-set-key (kbd "C-c C-s") 'split-window-horizontally)
+
+(global-set-key (kbd "C-M-<down>") 'scroll-up-command)
+
+(global-set-key (kbd "C-M-<up>") 'scroll-down-command)
+
+(global-set-key (kbd "s-+") 'text-scale-increase)
+
+(global-set-key (kbd "s--") 'text-scale-decrease)
+
+(global-set-key (kbd "M-x") 'smex)
+
+(global-set-key (kbd "M-.") 'mc/mark-all-like-this)
+
+(global-set-key (kbd "M-SPC") 'ace-jump-line-mode)
+
+(global-set-key (kbd "C-@") 'ace-jump-mode)
+
+(global-set-key (kbd "C-SPC") 'ace-jump-mode)
+
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+(global-set-key (kbd "M-d") 'comment-or-uncomment-region-or-line)
+
+(provide 'key-bindings)
---
+++ init.el
@@ -0,0 +1,77 @@
+;;folders
+(let ((default-directory "~/.emacs.d/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(require 'key-bindings)
+
+(require 'pbcopy)
+
+(require 'smex)
+(smex-initialize)
+
+(require 'lusty-explorer)
+
+(recentf-mode t)
+
+;;auto pair brackets
+(require 'autopair)
+(autopair-global-mode)
+
+;;line numbers
+(global-linum-mode 1)
+(setq linum-format " %d ")
+(column-number-mode 1)
+
+;;ace-jump-mode
+(require 'ace-jump-mode)
+
+;;matching braces
+(show-paren-mode 1)
+
+;;shift select up
+(if (equal "xterm-256color" (tty-type))
+    (define-key input-decode-map "\e[1;2A" [S-up]))
+
+;; No yes-or-no, y-or-n instead
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;;delete selected text with any key
+(delete-selection-mode t)
+
+;; Auto revert buffers
+(global-auto-revert-mode 1)
+
+;; revert buffer w/o asking
+(setq revert-without-query (quote (".*")))
+
+;;disable backup files
+(setq backup-inhibited t)
+;;disable auto save
+(setq auto-save-default nil)
+
+;;freaking whitespaces trail
+(defun cleanup-buffer-safe ()
+  (interactive)
+  (untabify (point-min) (point-max))
+  (delete-trailing-whitespace)
+  (set-buffer-file-coding-system 'utf-8))
+
+(add-hook 'before-save-hook 'cleanup-buffer-safe)
+
+;;camel case
+(add-hook 'prog-mode-hook 'subword-mode)
+
+;;disable menubar/scrollbar/tool-bar
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(custom-safe-themes (quote ("c17eba2c8a017959699591f0cbb4739afb3ebb891c0887e58ad95cf667859253" default)))
+ '(indent-tabs-mode nil)
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(text-mode-hook (quote (text-mode-hook-identify)))
+ '(tool-bar-mode nil))
