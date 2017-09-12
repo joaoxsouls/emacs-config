8c312f77c5e61e8eb04b9f1dd35a3698da02366d
João Oliveira <hello@jxs.pt>
2017-09-12 04:07:56 +0100

    add .editorconfig file

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)      
+(package-initialize)
 
 (let ((default-directory "~/.emacs.d/"))
   (normal-top-level-add-subdirs-to-load-path))
